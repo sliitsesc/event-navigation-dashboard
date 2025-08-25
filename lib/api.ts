3298423277b22e48b2d577df7ea40f1b1b670056
saputhebeast <sapumalwijekoon4@gmail.com>
2025-08-26 00:18:13 +0530

    Feat: deployed url

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import type { User, Zone, Stall, ApiResponse, CreateZoneData, CreateStallData } from "@/types"
 
 class ApiClient {
-  private baseURL = "http://localhost:9090"
+  private baseURL = "http://34.121.251.7:9090"
 
   private async request<T>(endpoint: string, options: RequestInit = {}): Promise<ApiResponse<T>> {
     const user = this.getStoredUser()
